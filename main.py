e7b4e91a2f2333bb58a183fd6bfcaf13871b924e
connerCpowell <copowengineering@gmail.com>
2025-07-13 20:58:35 -0700

    handle fhir.json

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.ingest_clean import load_data, clean_data
 
-df = load_data('..')
+df = load_data('./data/raw/20k/Aleta47_Lala778_Kovacek682_d7183866-d578-f362-a968-14acc858ee80.json')
 df = clean_data(df)
 
-df.to_csv('data/cleaned/cleaned_CD.json')
+df.to_csv('data/clean/cleaned_CD.json')
--- src/ingest_clean.py
+++ src/ingest_clean.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 PII_COLUMNS = [
     'name',
@@ -9,16 +10,30 @@ PII_COLUMNS = [
     'social_security_number',
     'email',
     'phone',
-    'dob',
-    'date_of_birth',
-    'address',
-    'street',
-    'zip',
-    'zip_code',
-    'city',
-    'state',
-    'mrn',  # medical record number
+    #'dob',
+    #'date_of_birth',
+    #'address',
+    #'street',
+    #'zip',
+    #'zip_code',
+    #'city',
+    #'state',
+    #'mrn',  # medical record number
 ]
+
+
+def flatten_file(jfile):
+    flat = {}
+    def _flatten(obj, prefix=''):
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                _flatten(v, f"{prefix}{k}_" if prefix else k + "_")
+        elif isinstance(obj, list):
+            flat[prefix.rstrip('_')] = str(obj)
+        else:
+            flat[prefix.strip('_')] = obj
+    _flatten(jfile)
+    return flat
 
 def load_data(path: str) -> pd.DataFrame:
     if path.endswith('.csv'):
@@ -29,6 +44,24 @@ def load_data(path: str) -> pd.DataFrame:
         return pd.read_json(path)
     else:
         raise ValueError("Unexpected format -- brrrr")
+    
+def load_fhir_json(path: str) -> pd.DataFrame:
+    with open(path, 'r') as f:
+        data = json.load(f)
+
+    good_cols = ['Patient', 'Encounter', 'Condition', 'Claim', 'DiagnosticReport']
+
+    flat_rec = []
+    for entry in data.get('entry', []):
+        res = entry.get('resource', {})
+        if res.get('resourceType') in good_cols:
+            flat = flatten_file(res)
+            flat_rec.append(flat)
+
+    df = pd.DataFrame(flat_rec)
+    return df
+
+            
     
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
